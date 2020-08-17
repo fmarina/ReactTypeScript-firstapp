b5b2bf2be57879f3caaa06a5af597fb0c546102e
Marina <marinafaenze@hotmail.com>
2020-08-17 11:29:06 -0300

    list all tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,35 @@
-import React, {useState, Fragment, FormEvent} from 'react';
-
+import React, {useState} from 'react';
  //React esto es un form event y esto viene por parte de un elemento de html for element
 type FormElement = React.FormEvent<HTMLFormElement>;
+interface ITask {
+  name: string,
+  done: boolean
+}
 
 
 function App(): JSX.Element {
 
   const [newTask, setNewTask] = useState<string>("");
-  
+  const [tasks, setTasks] = useState<ITask[]>([]);
+
+  const handleSubmit = (e: FormElement) => {
+    e.preventDefault();
+    addTasks(newTask);
+    setNewTask("");
+  }
+
+  const addTasks = (name: string) => {
+    const newTasks: ITask[] = [...tasks, {name, done: false}];
+    setTasks(newTasks);
+  }
+
   return (
     <>
-      <form>
-        <input type="text" onChange={(e) => setNewTask(e.target.value)} />
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={(e) => setNewTask(e.target.value)} value={newTask} />
         <button>Guardar</button>
       </form>
+      { tasks.map((task: ITask, i: number) => <h1 key={i}>{task.name}</h1>) }
     </>
   );
 }
